ad5ca52c9c3916b168d088eac55c1845e84b4022
CAM603 <cameronhawley@comcast.net>
2020-03-17 10:08:02 -0400

    ADD images

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/Card';
 
 export default function Home({ navigation }) {
     const [reviews, setReviews] = useState([
         {title: 'Residential Evils 10', rating: 5, body: 'Just like the original!', key: '1'},
-        {title: 'Red Dead Premonition', rating: 4.5, body: 'So scary!', key: '2'},
+        {title: 'Red Dead Premonition', rating: 4, body: 'So scary!', key: '2'},
         {title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'Biggest Zelda game so far!', key: '3'}
     ])
     
@@ -15,7 +16,9 @@ export default function Home({ navigation }) {
                 data={reviews}
                 renderItem={({ item }) => (
                     <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', {item})}>
-                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                        <Card>
+                            <Text style={globalStyles.titleText}>{item.title}</Text>
+                        </Card>
                     </TouchableOpacity>
                 )}
             />
